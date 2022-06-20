0c5908d694c61531127638a03f444155afd97831
Walid <walidhaddoury@outlook.fr>
2022-06-20 10:59:04 +0200

    programme année bisextile fini

---
+++ walid.py
@@ -0,0 +1,14 @@
+import sys;
+
+
+while True:
+    year = int(input("Entrez une année : "))
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        print("L'année est bissextile")
+    else:
+        print("L'année n'est pas bissextile")
+    again = input("Voulez vous continuer ? oui / non : ")
+    if again == "non":
+        break
+    else:
+        continue
