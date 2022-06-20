a8b865902b2853dca2f72da5454a6af7e47a1c7a
GauthierCornec <70896162+GauthierCornec@users.noreply.github.com>
2022-06-20 16:32:29 +0200

    fix doc string

--- exo2.py
+++ exo2.py
@@ -1,6 +1,31 @@
 letters = str(input("Entrez string:"))
 
-def split_string(letters):
-    string = "A B C"
-    arr = string.split()
-    print(arr)
+'''
+params letter 
+return array string ["", "" ]
+'''
+def split_letters(string):
+    return list(string)
+
+'''
+params letter 
+return array ascii  [66, 65, ... ]
+'''
+def convert_letter_to_ascii(string):
+    array = []
+    for letter in split_letters(string):
+        array.append(ord(letter))
+    return array
+
+'''
+params letter 
+return array binary ["", "" ]
+'''
+def to_binary(string):
+    array1, array2 = convert_letter_to_ascii(string), []
+    for i in array1:
+        array2.append(int(bin(i)[2:]))  # [2:] Slice two first characters from the beginning
+    return array2
+
+
+print(to_binary(letters))
