d3773d07feeee4b5ed84e5e09d5de2de03ff5fb5
Walid <walidhaddoury@outlook.fr>
2022-06-21 10:47:51 +0200

    add of doc string

--- walid/exo2_walid.py
+++ walid/exo2_walid.py
@@ -2,6 +2,11 @@ test = ["1000001", "1000011", "100001"]
 
 
 def complete_octet(array_of_binary):
+    """
+    Complete octet if don't have 8 caractere
+    :param array_of_binary: array with all octet in binary
+    :return: array with octet completed with zero
+    """
     for index, octet in enumerate(array_of_binary):
         if len(octet) < 8:
             nb_of_zero = 8 - len(octet)
@@ -13,6 +18,11 @@ def complete_octet(array_of_binary):
 
 
 def concat_multiple_string(array):
+    """
+    Transform array of octet on string of all octet concated
+    :param array: all octet
+    :return: string
+    """
     concat = ''.join(array)
     return concat
 
